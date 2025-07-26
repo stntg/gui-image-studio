743a842c2bcca10a6a16e86c6b0c5dcbb3fd8e49
Stan <stantgriffiths@gmail.com>
2025-07-26 13:19:31 +0100

    Update test_package.py

--- tests/test_package.py
+++ tests/test_package.py
@@ -10,7 +10,7 @@ import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "src"))
 
 import gui_image_studio
-from gui_image_studio.generator import embed_images_from_folder
+from gui_image_studio.core.code_generation import embed_images_from_folder
 from gui_image_studio.sample_creator import create_sample_images
 
 
